456cfa7c45758f9e1b5c62225f83d4f1a66fcf50
Zarn Bou <Zarn Bou>
2017-03-29 13:40:47 +1100

    Add post todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+})
+
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
+
+
+
+// var newTodo = new Todo({
+//     text: 'Go to gym',
+//     completed: false,
+//     completedAt: 8
+// });
+
+// var otherTodo = new Todo({
+//     text: '    Edit this video'
+// });
+
+// otherTodo.save().then((doc) => {
+//     console.log('Saved todo', doc);
+// }, (e) => {
+//     console.log('Unable to save todo', e);
+// });
+
+// var newUser = new User({
+//     email: '   zarn.bou@gmail.com  '
+// });
+
+// newUser.save().then((doc) => {
+//     console.log('Saved user', doc);
+// }, (e) => {
+//     console.log('Unable to save user', e);
+// });
