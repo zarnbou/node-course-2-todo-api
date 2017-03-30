c75f67e17f9957c46b04e8fa5202993a57cefdbd
Zarn Bou <Zarn Bou>
2017-03-30 16:29:11 +1100

    add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo.js');
 
 const todos = [{
+        _id: new ObjectID(),
         text: 'First test todo'
     },
     {
+        _id: new ObjectID(),
         text: 'Second test todo'
     }];
 
@@ -75,6 +78,33 @@ describe('GET /todos', () => {
             .expect((res) => {
                 expect(res.body.todos.length).toBe(2);
             })
+            .end(done);
+    });
+});
+
+describe('GET /todos:id', () => {
+    it('should return 200 with valid Object Id and does exist', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    })
+
+    it('should return 404 with invalid Object Id', (done) => {
+        request(app)
+            .get('/todos/abc123') //invalid object id
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 with valid Object Id but does not exist', (done) => {
+        var hexId = new ObjectID().toHexString();
+        request(app)
+            .get(`/todos/${hexId}`)
+            .expect(404)
             .end(done);
     });
 });
