96795d805d2dba7276e3a1b1a0844f003e0e6c43
Zarn Bou <Zarn Bou>
2017-03-30 16:03:14 +1100

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -28,7 +29,22 @@ app.get('/todos', (req, res) => {
     });
 });
 
-
+// GET /todos/234242424
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo: todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
